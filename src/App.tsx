da40c6f029379d5fe4cd5cb404634eb87df07c93
AlexKovalovSHK <alex@shk-info.de>
2026-03-14 15:33:12 +0100

    update content

--- src/App.tsx
+++ src/App.tsx
@@ -238,7 +238,7 @@ function App() {
             <FadeInSection>
               <h2 className="text-center mb-5">{t("section-github")}</h2>
               <div className="list-group max-w-75 mx-auto" style={{ maxWidth: '800px' }}>
-                <a href="https://github.com/alexshmidt/microservices-core" target="_blank" className="list-group-item list-group-item-action p-4 border-light shadow-sm mb-3 rounded">
+                <a href="https://github.com/AlexKovalovSHK/ifob_scool" target="_blank" className="list-group-item list-group-item-action p-4 border-light shadow-sm mb-3 rounded">
                   <div className="d-flex w-100 justify-content-between align-items-center mb-2">
                     <h5 className="mb-1 text-primary d-flex align-items-center gap-2">
                       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
